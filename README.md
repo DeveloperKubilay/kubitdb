3a5bce2d5994ea0ccf964c636a4692faab9d3766
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2022-06-11 18:13:01 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,168 +1,101 @@
-# KubitDBonline
-Versiyon: **kubitdbonline@1.0.4**
-Modülün sahibi: **kubi#5443**
+# KubitDB
+Yeni versiyon **kubitdb@1.4.8**
+Modülün sahibi **kubi#5443**
 ## Bilgiler 
 **Yardım:** [Yardım sunucusu](https://discord.gg/4Xpwwz6pgN) 
-# Kullanımı
-## Sunucu açma
-
+### Kullanımı
 ```js
-const parola = "sifre"
-const loglimit = "1mb"
-const dosyaismi = "kubitdbonline"
-const port = 80
-
 const {kubitdb} = require('kubitdb')
-const db = new kubitdb(dosyaismi)
-const express = require('express')
-const app = express()
-app.listen(port)
-const bodyParser = require("body-parser")
-app.use(bodyParser.json({limit: loglimit}));
-app.use(bodyParser.urlencoded({limit: loglimit, extended: true}));
+const db = new kubitdb("kubitdb")//kaydeceği dosyanın ismi değiştirebilirsiniz
+//İsterseniz const db = new kubitdb("./kubitdb.json") olarak kullanabilirsiniz
 
-app.get('/kubitdbonlineall/'+parola, function (req, res) {res.json(db.hepsi())})
-app.post('/kubitdbonline', function (req, res) {
-if (parola === req.body.parola) {
-if(req.body.yapilcak === "set"){db.ayarla(req.body.deger1,req.body.deger2)}
-if(req.body.yapilcak === "add"){db.ekle(req.body.deger1,req.body.deger2)}
-if(req.body.yapilcak === "delete"){db.sil(req.body.deger1)}
-if(req.body.yapilcak === "subtract"){db.cıkar(req.body.deger1,req.body.deger2)}
-if(req.body.yapilcak === "push"){db.it(req.body.deger1,req.body.deger2)}
-if(req.body.yapilcak === "clear"){db.temizle()}  
-res.end()
-}})
-```
-## Sunucuya bağlanma
-```js
-const kubitdbonline = require('kubitdbonline')
-const db = new kubitdbonline("https://slyhterindashboard.cf","sifre")
-
-//Sunucuda Bilgi alma (fetch,get,all)
-db.al()
-db.bak()
-db.get()
-db.fetch()
-
-const kubitdbonline = require('kubitdbonline')
-const db = new kubitdbonline("https://slyhterindashboard.cf","sifre")
-db.al().then(kubitdb=>{
-console.log(kubitdb)//if you want all data 
-console.log(kubitdb["elma"])//elmayı değiştirebilirsiniz
-})
-})
-/*/
-3
-/*/
-
-//Sunucuda varmı (has)
-db.al()
-db.bak()
-db.get()
-db.fetch()
-
-const kubitdbonline = require('kubitdbonline')
-const db = new kubitdbonline("https://slyhterindashboard.cf","sifre")
-
-db.al().then(kubitdb=>{
-var has;if(kubitdb["elma"]) {has = true} else {has = false}
-console.log(has)
-})
-/*
-true
-false
-*/
-
-//Sunucuda ayarlama (set)
-db.ayarla()
-db.set()
-
-const kubitdbonline = require('kubitdbonline')
-const db = new kubitdbonline("https://slyhterindashboard.cf","sifre")
-db.ayarla("elma","yok") 
+db.ayarla('deneme','açık')
+db.set('deneme','true')
 /*/
 {
-  "elma":"yok"
+  "deneme": "açık"
 }
 /*/
 
-//Sunucuda silme (delete)
-db.sil()
-db.delete()
-db.del()
-
-const kubitdbonline = require('kubitdbonline')
-const db = new kubitdbonline("https://slyhterindashboard.cf","sifre")
-db.sil("elma") 
+db.ekle("para", 31)
+db.add("para", 31)
+/*/
+{
+  "para": 31
+}
+/*/
+db.it("serverSettings", { whitelist: "on", playerCount: "12" })
+db.push("serverSettings", { whitelist: "on", playerCount: "12" })
+/*/
+"serverSettings": [
+    {
+      "whitelist": "on",
+      "playerCount": "12"
+    }
+  ]
+/*/
+db.varmı("prefix") 
+db.has("prefix") 
+/*/
+true
+false
+/*/
+db.al("para")
+db.bak("para")
+db.get("para")
+db.fetch("para")
+/*/
+500
+/*/
+db.cıkar("para", 400)
+db.subtract("para", 400)
+db.substr("para", 400)
+/*/
+100
+/*/
+db.sil("serverStatus")
+db.delete("serverStatus")
 /*/
 {}
 /*/
-
-//Sunucuda temizleme (clear)
+db.sil("serverStatus",1)
+db.delete("serverStatus",100)
+/*/
+{
+  "serverStatus": 9
+}
+/*/
+db.hepsi()
+db.hepsinial()
+db.all()
+db.fetchAll()
+db.getAll()
+/*/
+{
+  money: 500,
+  deneme: "abc"
+}
+/*/
 db.temizle()
 db.clear()
 db.deleteAll()
 db.clearAll()
-
-const kubitdbonline = require('kubitdbonline')
-const db = new kubitdbonline("https://slyhterindashboard.cf","sifre")
-db.temizle()
 /*/
 {}
 /*/
 
-//Sunucuda cıkarma (subtract)
-db.cıkar()
-db.subtract()
-
-const kubitdbonline = require('kubitdbonline')
-const db = new kubitdbonline("https://slyhterindashboard.cf","sifre")
-db.cıkar("elma",1) 
+//hesapla ve math data'ya kaydetmez db.al,db.fetch gibidir
+db.math("elma","+",10)
+db.hesapla("elma","+",10)
 /*/
-{
-  "elma": "9"
-}
+10
 /*/
-
-//Sunucuda itme (push)
-db.it()
-db.push()
-
-const kubitdbonline = require('kubitdbonline')
-const db = new kubitdbonline("https://slyhterindashboard.cf","sifre")
-db.it("elma","deneme")
-/*/
-{
-  "elma":{
-    "deneme",
-    {
-      deneme:"3",
-      "aa":"c"
-    }
-  }
-}
-/*/
-
-//Sunucuda ekleme (add)
-db.ekle()
-db.add()
-
-const kubitdbonline = require('kubitdbonline')
-const db = new kubitdbonline("https://slyhterindashboard.cf","sifre")
-db.ekle("elma",1)
-/*/
-{
-  "elma": 2
-}
-/*/
-
 ```
-### Yerel Database için
-##### KubitDB
-- ```npm i kubitdb```
-##### [KubitDB kullanımı](https://www.npmjs.com/package/kubitdb) 
-
-##### Hatamı alıyorsun konsola npm i axios express kubitdb body-parser yazmayı dene
-##### Hala hatamı alıyorsun [Yardım sunucumuza gel](https://discord.gg/4Xpwwz6pgN) 
-##### KubitDBonline'yi Yüklemek için
+### Online Database için
+##### KubitDBonline
 - ```npm i kubitdbonline```
+##### [KubitDB online kullanımı](https://www.npmjs.com/package/kubitdbonline) 
+
+##### Hatamı alıyorsun [Yardım sunucumuza gel](https://discord.gg/4Xpwwz6pgN) 
+##### KubitDB'yi Yüklemek için
+- ```npm i kubitdb```
--- index.js
+++ index.js
@@ -1,92 +1,215 @@
-//Modül version 1.0.4
+//Modül sürümü: 1.4.8
 
 //Modül MİT lisanslıdır
-//axios modülünün yardımı ile çalışmaktadır ve bu modülün sahibi: Discord kubi#5443
-//Hatamı alıyorsun konsola npm i axios yazmayı dene
-//Hala hatamı alıyorsun Yardım sunucumuza gel https://discord.gg/4Xpwwz6pgN
+//fs modülünün yardımı ile çalışmaktadır ve sahibi: Discord kubi#5443
+//Halamı hata alıyorsun ozmn discord sunucumuza gel, ticket aç ve sorunu bize bildir 
+//Discord sunucumuz https://discord.gg/4Xpwwz6pgN
 //İyi günler İyi kodlamalar :)
 
-const fetch = require('axios')
-module.exports = class kubitdbonline {
-constructor(urlcik,sifre){
-this.url = urlcik
-if(!this.url) throw new TypeError("KubitDB online\nUrl girmediniz\nYou did not enter the url\n" + __dirname)
-this.sifre = sifre
-if(!this.sifre) throw new TypeError("KubitDB online\nŞifre girmediniz\nYou did not enter a password\n" + __dirname)
-}
+const fs = require('fs');
+const load = (file) => JSON.parse(fs.readFileSync(file, 'utf-8'));
+const write = (file, data) => fs.writeFileSync(file, JSON.stringify(data, undefined, 4));
+const extension = (filePath) => { let parts = filePath.split('.'); return parts[parts.length - 1]; }
+class kubitdb {constructor(file) {
+this.file = file || 'kubitdb.json'
+if (this.file === 'kubitdb.json') { try { load(this.file); } catch { write(this.file, {}); } } else {
+if(extension(this.file) !== 'json') this.file = this.file + '.json'
+if (!this.file.includes('./')) {this.file = './' + this.file}
+try { load(this.file); } catch { write(this.file, {});}}}
 
-set(veri, deger) {
-if (!veri || !deger) return;
-fetch.post(`${this.url}/kubitdbonline`,{parola:this.sifre,yapilcak:"set",deger1:veri,deger2:deger}).catch(() => {return;})
-return;}
-ayarla(veri, deger) {
-if (!veri || !deger) return;
-fetch.post(`${this.url}/kubitdbonline`,{parola:this.sifre,yapilcak:"set",deger1:veri,deger2:deger}).catch(() => {return;})
-return;}
-
-fetch () {
-const api = fetch.get(`${this.url}/kubitdbonlineall/`+this.sifre).then(res=>res.data).catch(() => {return{}})
-return api}
-get() {
-const api = fetch.get(`${this.url}/kubitdbonlineall/`+this.sifre).then(res=>res.data).catch(() => {return{}})
-return api;}
-al() {
-const api = fetch.get(`${this.url}/kubitdbonlineall/`+this.sifre).then(res=>res.data).catch(() => {return{}})
-return api;}
-bak() {
-const api = fetch.get(`${this.url}/kubitdbonlineall/`+this.sifre).then(res=>res.data).catch(() => {return{}})
-return api;}
-
-sil(veri) {
-if (!veri) return;
-fetch.post(`${this.url}/kubitdbonline`,{parola:this.sifre,yapilcak:"delete",deger1:veri}).catch(() => {return;})
-return;}
-delete(veri) {
-if (!veri) return;
-fetch.post(`${this.url}/kubitdbonline`,{parola:this.sifre,yapilcak:"delete",deger1:veri}).catch(() => {return;})
-return;}
-del(veri) {
-if (!veri) return;
-fetch.post(`${this.url}/kubitdbonline`,{parola:this.sifre,yapilcak:"delete",deger1:veri}).catch(() => {return;})
+set(data, value) {
+if (!data) return;
+if (!value) return;
+let fileData = load(this.file)
+fileData[data] = value;
+write(this.file, fileData);
 return;}
 
-cıkar(veri,miktar) {
-if(!miktar || !veri) return;
-fetch.post(`${this.url}/kubitdbonline`,{parola:this.sifre,yapilcak:"subtract",deger1:veri,deger2:miktar}).catch(() => {return;})
-return;}
-subtract(veri,miktar) {
-if(!miktar || !veri) return;
-fetch.post(`${this.url}/kubitdbonline`,{parola:this.sifre,yapilcak:"subtract",deger1:veri,deger2:miktar}).catch(() => {return;})
-return;}
-
-it(veri,deger) {
-if (!deger || !veri) return;
-fetch.post(`${this.url}/kubitdbonline`,{parola:this.sifre,yapilcak:"push",deger1:veri,deger2:deger}).catch(() => {return;})
-return;}
-push(veri,deger) {
-if (!deger || !veri) return;
-fetch.post(`${this.url}/kubitdbonline`,{parola:this.sifre,yapilcak:"push",deger1:veri,deger2:deger}).catch(() => {return;})
+ayarla(data, value) {
+if (!data) return;
+if (!value) return;
+let fileData = load(this.file)
+fileData[data] = value;
+write(this.file, fileData);
 return;}
 
-ekle(veri,deger) {
-if (!deger || !veri) return;
-fetch.post(`${this.url}/kubitdbonline`,{parola:this.sifre,yapilcak:"add",deger1:veri,deger2:Number(deger)}).catch(() => {return;})
-return;}
-add(veri,deger) {
-if (!deger || !veri) return;
-fetch.post(`${this.url}/kubitdbonline`,{parola:this.sifre,yapilcak:"add",deger1:veri,deger2:Number(deger)}).catch(() => {return;})
+add(data, value) {
+if (!data) return;
+if (!value) return;
+if (typeof value == "number") {let fileData = load(this.file)
+if (fileData[data] === undefined) return this.set(data, value);
+if (typeof fileData[data]!== "number") return;
+if (isNaN(fileData[data])) return this.set(data, value);
+fileData[data] = fileData[data] + value;
+write(this.file, fileData);
+return;} else {let fileData = load(this.file)
+if (typeof fileData[data]!== "number") return;
+if (fileData[data] === undefined) return;
+if (isNaN(fileData[data])) return;
+fileData[data] = fileData[data] + value;
+write(this.file, fileData);
+return;}}
+
+ekle(data, value) {
+if (!data) return;
+if (!value) return;
+if (typeof value == "number") {let fileData = load(this.file)
+if (fileData[data] === undefined) return this.set(data, value);
+if (typeof fileData[data]!== "number") return;
+if (isNaN(fileData[data])) return this.set(data, value);
+fileData[data] = fileData[data] + value;
+write(this.file, fileData);
+return;} else {let fileData = load(this.file)
+if (typeof fileData[data]!== "number") return;
+if (fileData[data] === undefined) return;
+if (isNaN(fileData[data])) return;
+fileData[data] = fileData[data] + value;
+write(this.file, fileData);
+return;}}
+
+subtract(data, value) {
+if (!data) return;
+if (!value) return;
+let fileData = load(this.file)
+if (fileData[data] === undefined) return;
+if (isNaN(fileData[data]-value)) return;
+fileData[data] = fileData[data]-value
+write(this.file, fileData);
 return;}
 
-temizle() {
-fetch.post(`${this.url}/kubitdbonline`,{parola:this.sifre,yapilcak:"clear"}).catch(() => {})
+substr(data, value) {
+if (!data) return;
+if (!value) return;
+let fileData = load(this.file)
+if (fileData[data] === undefined) return;
+if (isNaN(fileData[data]-value)) return;
+fileData[data] = fileData[data]-value
+write(this.file, fileData);
 return;}
-clearAll() {
-fetch.post(`${this.url}/kubitdbonline`,{parola:this.sifre,yapilcak:"clear"}).catch(() => {})
+
+cıkar(data, value) {
+if (!data) return;
+if (!value) return;
+let fileData = load(this.file)
+if (fileData[data] === undefined) return;
+if (isNaN(fileData[data]-value)) return;
+fileData[data] = fileData[data]-value
+write(this.file, fileData);
 return;}
-deleteAll() {
-fetch.post(`${this.url}/kubitdbonline`,{parola:this.sifre,yapilcak:"clear"}).catch(() => {})
+
+push(array, value) {
+if (!array) return;
+if (!value) return;
+let fileData = load(this.file)
+if (fileData[array] && Array.isArray(fileData[array])) {
+fileData[array].push(value)
+write(this.file, fileData)} else if (!fileData[array]) { this.set(array, [value]) }
 return;}
-clear() {
-fetch.post(`${this.url}/kubitdbonline`,{parola:this.sifre,yapilcak:"clear"}).catch(() => {})
+
+it(array, value) {
+if (!array) return;
+if (!value) return;
+let fileData = load(this.file)
+if (fileData[array] && Array.isArray(fileData[array])) {
+fileData[array].push(value)
+write(this.file, fileData)
+} else if (!fileData[array]) { this.set(array, [value]) }
 return;}
-}
+
+delete(array, index) {
+let fileData = load(this.file)
+if(!fileData[array]) return;
+if (!array) return;
+if (index === undefined) {
+fileData[array] = undefined;
+write(this.file, fileData);} else {
+let fileData = load(this.file)
+if (isNaN(fileData[array]-index)) return;
+fileData[array] = fileData[array] - index;
+write(this.file, fileData);} return;}
+
+sil(array, index) {
+let fileData = load(this.file)
+if(!fileData[array]) return;
+if (!array) return;
+if (index === undefined) {
+fileData[array] = undefined;
+write(this.file, fileData);} else {
+let fileData = load(this.file)
+if (isNaN(fileData[array]-index)) return;
+fileData[array] = fileData[array] - index;
+write(this.file, fileData);} return;}
+  
+has(data) {
+if (!data) return;
+let fileData = load(this.file)
+if (!fileData[data]) return false;
+if (fileData[data]) return true;}
+
+varmı(data) {
+if (!data) return;
+let fileData = load(this.file)
+if (!fileData[data]) return false;
+if (fileData[data]) return true;}
+
+clear() { write(this.file, {}); return; }
+clearAll() { write(this.file, {}); return; }
+temizle() { write(this.file, {}); return; }
+deleteAll() { write(this.file, {}); return; }
+
+fetchAll() { return load(this.file); }
+getAll() { return load(this.file); }
+all() { return load(this.file); }
+hepsi() { return load(this.file); }
+hepsinial() { return load(this.file); }
+
+fetch(data) {
+if (!data) return;
+let fileData = load(this.file)
+if (!fileData[data]) fileData[data] = undefined
+return fileData[data]}
+
+get(data) {
+if (!data) return;
+let fileData = load(this.file)
+if (!fileData[data]) fileData[data] = undefined
+return fileData[data]}
+
+bak(data) {
+if (!data) return;
+let fileData = load(this.file)
+if (!fileData[data]) fileData[data] = undefined
+return fileData[data]}
+
+al(data) {
+if (!data) return;
+let fileData = load(this.file)
+if (!fileData[data]) fileData[data] = undefined
+return fileData[data]}
+
+hesapla(data, operator, value) {
+if (!data) return;
+if (!operator) return;
+if (!value) return;
+if (typeof value !== "number") return;
+let fileData = load(this.file)
+if (typeof fileData[data] !== "number") return;
+if (operator === "-") {let i = fileData[data] - value;
+return i} else if (operator === "+") {let ii = fileData[data] + value;
+return ii} else if (operator === "*") {let iii = fileData[data] * value;
+return iii} else if (operator === "/") {let iiii = fileData[data] / value;
+return iiii} else { return; }}
+
+math(data, operator, value) {if (!data) return;
+if (!operator) return;
+if (!value) return;
+if (typeof value !== "number") return;
+let fileData = load(this.file)
+if (typeof fileData[data] !== "number") return;
+if (operator === "-") {let i = fileData[data] - value;
+return i} else if (operator === "+") {let ii = fileData[data] + value;
+return ii} else if (operator === "*") {let iii = fileData[data] * value;
+return iii} else if (operator === "/") {let iiii = fileData[data] / value;
+return iiii} else { return; }}
+
+}module.exports = { kubitdb };
